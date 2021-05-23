d25186a5435c3bf11c7c0e1e504329bd4dc526cb
Akshat Patel <akshat365@gmail.com>
2021-05-22 18:51:47 -0700

    Design our algorithm in Brainstorming.txt

--- router.cpp
+++ router.cpp
@@ -1,10 +1,24 @@
 // Author Sean Davis
 #include "router.h"
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+void Router::printMap()
+{
+    for(int i = 0; i < 1000; i++)
+    {
+        for(int j = 0; j < 1000; j++)
+        {
+            cout << map_[i][j] << " ";
+        }
+    }
+}
 
 Router::Router(const Map1000 *map, int width)
 {
+    memcpy(&(map_), &(map->map1000), sizeof(map_));   
+    printMap(); 
 } // Router()
 
 Router::~Router()
--- router.h
+++ router.h
@@ -8,8 +8,10 @@
 class Router
 {
 public:
+  short map_[1000][1000];
   Router(const Map1000 *map, int width);
   ~Router();
+  void printMap();
   void findRoutes(const CityPos *cityPos, int cityCount, Edge *paths, int &pathCount);
 };
 
